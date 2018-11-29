f4dd4c96ea0990cb13ad92d438263be5f5ae62b7
Hannah Neal <hannahmneal@gmail.com>
2018-11-29 13:31:07 -0600

    set window.scrollY but still need to set min- height and width

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-
+const audrey = document.getElementById("audrey")
 /*
     Add an event listener to the `document` object to listen
     for the "scroll" event.
 */
 
-let sectionEl = document.addEventListener("scroll", function (audreyWidth, audreyHeight) {
+let audreyScroll = document.addEventListener("scroll", function () {
     /*
         Adjust the width of audrey to be 1/3 the value of
         `window.scrollY`. No lower than 50px, though.
     */
-    
-    let audreyWidth = document.queryGetElementById("#audrey").window.scrollY; 
-    // let audreyWidth = document.querySelector("#audrey").width.value; 
+    // let audreyWidth = document.getElementById("container").style.minWidth = "50px";
+    // window.scrollY = container
+    // min-height of container = 1500px
+    // 50px > width of audrey  < 33% of container 
+    // 100px > height of audrey < 25% of container
+
+    // let audreyHeight = document.getElementById("container").style.minWidth = "100px";
 
     /*
         Adjust the height of audrey to be 1/4 the value of
         `window.scrollY`. No lower than 100px, though.
     */
-})
 
+    audrey.style.width = window.scrollY + "px"
+    audrey.style.height = window.scrollY + "px"
+
+
+})
